2c01897670b0cc4d72eea4bb153238e9a7b20c23
vsdiaman <vitor.diamantino8@gmail.com>
2023-03-13 19:08:22 -0300

    feat: confing .env e index para manter a seguranca da aplicacao

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 // const Person = require("./models/Person");
@@ -8,12 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+//Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
 const personRoutes = require("./routes/personRoutes");
 app.use("/person", personRoutes);
-const DB_USER = "vitordiamantino";
-const DB_PASSWORD = encodeURIComponent("Dn8FYGMy6m4zkbGB");
-
+const USER_DB = process.env.DB_USER;
+const PASS_DB = process.env.DB_PASSWORD;
+const PORT_BACK = process.env.DB_PORT_BACK;
 // Rota inicial / endpoint
+
+// Routes
+
 
 app.get("/", (req, res) => {
   // Mostrar a req
@@ -22,11 +30,13 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster0.0ffk5gy.mongodb.net/bancodaapiretryWrites=true&w=majority`
+    `mongodb+srv://${USER_DB}:${encodeURIComponent(
+      PASS_DB
+    )}@apicluster0.0ffk5gy.mongodb.net/bancodaapiretryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000, function () {
-      console.log("Server MongoDb is running on port " + 3000);
+    app.listen(PORT_BACK, function () {
+      console.log("Server MongoDb is running on port " + PORT_BACK);
     });
   })
   .catch((err) => console.log("Error na aplicação", err));
